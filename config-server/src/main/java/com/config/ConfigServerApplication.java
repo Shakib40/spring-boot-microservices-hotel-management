452bf0eb2fb09f900c84ee0039cbef1043320207
Shakib.jilani <shakib.jilani@sakhaglobal.com>
2025-09-29 14:53:28 +0530

    API

--- config-server/src/main/java/com/config/ConfigServerApplication.java
+++ config-server/src/main/java/com/config/ConfigServerApplication.java
@@ -1,15 +1,22 @@
 package com.config;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-public class ConfigServerApplication {
+public class ConfigServerApplication implements CommandLineRunner  {
 
 	public static void main(String[] args) {
+		System.out.println("✅✅✅ CONFIG-SERVER IS RUNNING ✅✅✅");
 		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) {
+		System.out.println("✅✅✅ CONFIG-SERVER is up and ready! ✅✅✅");
 	}
 
 }
