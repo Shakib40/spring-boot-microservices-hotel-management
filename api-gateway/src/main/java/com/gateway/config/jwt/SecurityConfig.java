9541d2422bc416fb0ab9f5f75f224a1f987188dd
Shakib Jilani <shakibjilani@gmail.com>
2025-10-24 17:58:09 +0530

    FF

--- api-gateway/src/main/java/com/gateway/config/jwt/SecurityConfig.java
+++ api-gateway/src/main/java/com/gateway/config/jwt/SecurityConfig.java
@@ -1,30 +1,7 @@
-// package com.gateway.config.jwt;
-
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-// import org.springframework.security.config.web.server.ServerHttpSecurity;
-// import org.springframework.security.web.server.SecurityWebFilterChain;
-
-// @Configuration
-// @EnableWebFluxSecurity
-// public class SecurityConfig {
-
-//     @Bean
-//     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
-//         System.out.println("✅ Security Config Loaded");
-//         return http
-//                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
-//                 .authorizeExchange(ex -> ex
-//                         .pathMatchers("/actuator/**", "/auth/**", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
-//                         .anyExchange().authenticated()
-//                 )
-//                 .build();
-//     }
-// }
+ 
 
 
-package com.gateway.config.jwt;
+ package com.gateway.config.jwt;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
