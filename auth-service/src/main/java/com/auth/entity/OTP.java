f02e5c5283123a25b925f9e021acf8cb4aa4ef6f
shakibjilani <shakib.jilani@sakhaglobal.com>
2026-02-17 16:34:13 +0530

    API

--- auth-service/src/main/java/com/auth/entity/OTP.java
+++ auth-service/src/main/java/com/auth/entity/OTP.java
@@ -2,6 +2,7 @@ package com.auth.entity;
 
 import java.time.LocalDateTime;
 
+// we can store the otp in the database for the user to verify the user as we are already using redis for the otp
 public class OTP {
     private String id;
     private String otp;
